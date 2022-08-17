46127b425c199de7735250c4b5c97bbc78c107a4
joe tech <ojtech2003@gmail.com>
2022-08-17 18:19:57 +0100

    FAQs

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,32 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("scrolled");
     }
 })
+
+// show or hide faq answers on click
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open");
+
+        // change icon
+        const icon = faq.querySelector(".faq__icon i");
+        if (icon.className === "bx bx-plus") {
+            icon.className = "bx bx-minus";
+        } else {
+            icon.className = "bx bx-plus";
+        }
+
+        // close other faqs if open and not the one clicked
+        
+        if (faq.classList.contains("open")) {
+            faqs.forEach(otherFaq => {
+                if (otherFaq !== faq) {
+                    otherFaq.classList.remove("open");
+                    otherFaq.querySelector(".faq__icon i").className = "bx bx-plus";
+                }
+            } )
+        }
+    })
+})
