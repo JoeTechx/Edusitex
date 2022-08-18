3d1b5b68899ac49e462f4e1f6bd32a6f9eb4f650
joe tech <ojtech2003@gmail.com>
2022-08-18 17:28:50 +0100

    testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", () => {
 
     // document.querySelector("nav").classList.toggle("scrolled", window.scrollY > 0);
     const navbar = document.querySelector("nav");
-    if (window.scrollY >0) {
+    if (window.scrollY > 0) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
@@ -13,28 +13,53 @@ window.addEventListener("scroll", () => {
 // show or hide faq answers on click
 
 const faqs = document.querySelectorAll(".faq");
+const accordions = document.querySelectorAll(".faq__header");
 
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("open");
 
+
         // change icon
-        const icon = faq.querySelector(".faq__icon i");
-        if (icon.className === "bx bx-plus") {
-            icon.className = "bx bx-minus";
+        const icon = faq.querySelector(".arrow__down i");
+        if (icon.className === "bx bxs-down-arrow") {
+            icon.className = "bx bxs-up-arrow";
         } else {
-            icon.className = "bx bx-plus";
+            icon.className = "bx bxs-down-arrow";
         }
 
         // close other faqs if open and not the one clicked
-        
+
         if (faq.classList.contains("open")) {
             faqs.forEach(otherFaq => {
                 if (otherFaq !== faq) {
                     otherFaq.classList.remove("open");
-                    otherFaq.querySelector(".faq__icon i").className = "bx bx-plus";
+                    otherFaq.querySelector(".arrow__down i").className = "bx bxs-down-arrow";
                 }
-            } )
+            })
+        }
+    })
+})
+
+accordions.forEach(accordion => {
+    accordion.addEventListener("click", () => {
+        accordion.classList.toggle("open__accordion");
+        // change icon
+        const icon = accordion.querySelector(".arrow__down i");
+        if (icon.className === "bx bxs-down-arrow") {
+            icon.className = "bx bxs-up-arrow";
+        } else {
+            icon.className = "bx bxs-down-arrow";
+        }
+
+        // close other faqs if open and not the one clicked
+        if (accordion.classList.contains("open__accordion")) {
+            accordions.forEach(otherAccordion => {
+                if (otherAccordion !== accordion) {
+                    otherAccordion.classList.remove("open__accordion");
+                    otherAccordion.querySelector(".arrow__down i").className = "bx bxs-down-arrow";
+                }
+            })
         }
     })
 })
