0c08ad52e1b9ffa5a4d4ae3839b5fc10fcaf7c4f
JoeTechx <oyinjoe23@gmail.com>
2022-08-24 04:43:56 -0700

    phone device

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,68 @@
 // change navbar style on scroll
 window.addEventListener("scroll", () => {
-
-    // document.querySelector("nav").classList.toggle("scrolled", window.scrollY > 0);
-    const navbar = document.querySelector("nav");
-    if (window.scrollY > 0) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-})
+  // document.querySelector("nav").classList.toggle("scrolled", window.scrollY > 0);
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
 
 // show or hide faq answers on click
 
 const faqs = document.querySelectorAll(".faq");
 const accordions = document.querySelectorAll(".faq__header");
 
-faqs.forEach(faq => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("open");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
 
+    // change icon
+    const icon = faq.querySelector(".arrow__down i");
+    if (icon.className === "bx bxs-down-arrow") {
+      icon.className = "bx bxs-up-arrow";
+    } else {
+      icon.className = "bx bxs-down-arrow";
+    }
 
-        // change icon
-        const icon = faq.querySelector(".arrow__down i");
-        if (icon.className === "bx bxs-down-arrow") {
-            icon.className = "bx bxs-up-arrow";
-        } else {
-            icon.className = "bx bxs-down-arrow";
+    // close other faqs if open and not the one clicked
+
+    if (faq.classList.contains("open")) {
+      faqs.forEach((otherFaq) => {
+        if (otherFaq !== faq) {
+          otherFaq.classList.remove("open");
+          otherFaq.querySelector(".arrow__down i").className =
+            "bx bxs-down-arrow";
         }
+      });
+    }
+  });
+});
 
-        // close other faqs if open and not the one clicked
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.classList.toggle("open__accordion");
+    // change icon
+    const icon = accordion.querySelector(".arrow__down i");
+    if (icon.className === "bx bxs-down-arrow") {
+      icon.className = "bx bxs-up-arrow";
+    } else {
+      icon.className = "bx bxs-down-arrow";
+    }
 
-        if (faq.classList.contains("open")) {
-            faqs.forEach(otherFaq => {
-                if (otherFaq !== faq) {
-                    otherFaq.classList.remove("open");
-                    otherFaq.querySelector(".arrow__down i").className = "bx bxs-down-arrow";
-                }
-            })
+    // close other faqs if open and not the one clicked
+    if (accordion.classList.contains("open__accordion")) {
+      accordions.forEach((otherAccordion) => {
+        if (otherAccordion !== accordion) {
+          otherAccordion.classList.remove("open__accordion");
+          otherAccordion.querySelector(".arrow__down i").className =
+            "bx bxs-down-arrow";
         }
-    })
-})
-
-accordions.forEach(accordion => {
-    accordion.addEventListener("click", () => {
-        accordion.classList.toggle("open__accordion");
-        // change icon
-        const icon = accordion.querySelector(".arrow__down i");
-        if (icon.className === "bx bxs-down-arrow") {
-            icon.className = "bx bxs-up-arrow";
-        } else {
-            icon.className = "bx bxs-down-arrow";
-        }
-
-        // close other faqs if open and not the one clicked
-        if (accordion.classList.contains("open__accordion")) {
-            accordions.forEach(otherAccordion => {
-                if (otherAccordion !== accordion) {
-                    otherAccordion.classList.remove("open__accordion");
-                    otherAccordion.querySelector(".arrow__down i").className = "bx bxs-down-arrow";
-                }
-            })
-        }
-    })
-})
+      });
+    }
+  });
+});
 
 // show or hide nav menu on click
 
@@ -71,17 +71,17 @@ const menuBtn = document.getElementById("open-menu-btn");
 const closeBtn = document.getElementById("close-menu-btn");
 
 if (menuBtn) {
-    menuBtn.addEventListener("click", () => {
-        menu.classList.add("show-menu");
-        closeBtn.classList.add("show-Btn");
-        menuBtn.classList.add("show-menuBtn");
-    });
+  menuBtn.addEventListener("click", () => {
+    menu.classList.add("show-menu");
+    closeBtn.classList.add("show-Btn");
+    menuBtn.classList.add("show-menuBtn");
+  });
 }
 
 if (closeBtn) {
-    closeBtn.addEventListener("click", () => {
-        menu.classList.remove("show-menu");
-        closeBtn.classList.remove("show-Btn");
-        menuBtn.classList.remove("show-menuBtn");
-    });
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+    closeBtn.classList.remove("show-Btn");
+    menuBtn.classList.remove("show-menuBtn");
+  });
 }
